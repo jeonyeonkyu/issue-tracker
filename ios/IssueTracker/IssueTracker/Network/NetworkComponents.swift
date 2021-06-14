d0a7df4ad5811b1d1972c0d3df02836646bfb7ac
Lia <kjk0825@postech.ac.kr>
2021-06-14 17:06:10 +0900

    feat: ✨networkManager 객체 생성(#78)

---
+++ ios/IssueTracker/IssueTracker/Network/NetworkComponents.swift
@@ -0,0 +1,39 @@
+//
+//  NetworkComponents.swift
+//  IssueTracker
+//
+//  Created by Lia on 2021/06/14.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case BadURL
+    case DecodingError(Error)
+    case EncodingError(Error)
+    case Unknown
+    case Status(Int)
+}
+
+enum HTTPMethodType {
+    static let get     = "GET"
+    static let post    = "POST"
+    static let put     = "PUT"
+    static let patch   = "PATCH"
+    static let delete  = "DELETE"
+}
+
+enum EndPoint {
+    static let scheme = "https"
+    static let host   = "issue-tracker-swagger.herokuapp.com"
+    
+    static func url(path: String) -> URL? {
+        var components = URLComponents()
+        
+        components.scheme = EndPoint.scheme
+        components.host = EndPoint.host
+        components.path = "\(path)"
+        
+        return components.url
+    }
+}
--- ios/IssueTracker/IssueTracker/Network/NetworkService.swift
+++ ios/IssueTracker/IssueTracker/Network/NetworkService.swift
@@ -8,4 +8,91 @@
 import Foundation
 import Combine
 
+protocol NetworkManageable {
+    func get<T: Decodable>(path: String, type: T.Type) -> AnyPublisher<T, Error>
+    func post<T: Encodable, R: Decodable>(path: String, data: T, result: R.Type) -> AnyPublisher<R, Error>
+}
+
+
+class NetworkManager {
+    
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+      self.session = session
+    }
+    
+}
+
+
+extension NetworkManager: NetworkManageable {
+    
+    func get<T: Decodable>(path: String, type: T.Type) -> AnyPublisher<T, Error> {
+        guard let url = EndPoint.url(path: path) else {
+            let error = NetworkError.BadURL
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+        return get(url: url, type: type)
+    }
+    
+    func post<T: Encodable, R: Decodable>(path: String, data: T, result: R.Type) -> AnyPublisher<R, Error> {
+        guard let url = EndPoint.url(path: path) else {
+            let error = NetworkError.BadURL
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+        return post(url: url, data: data, result: result)
+    }
+    
+}
+
+
+extension NetworkManager {
+    
+    private func get<T: Decodable>(url: URL, type: T.Type) -> AnyPublisher<T, Error> {
+        
+        return self.session.dataTaskPublisher(for: url)
+            .tryMap { element -> Data in
+                guard let httpResponse = element.response as? HTTPURLResponse else { throw NetworkError.Unknown}
+                let statusCode = httpResponse.statusCode
+                if statusCode != 200 { throw NetworkError.Status(statusCode) }
+                return element.data
+            }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .mapError({ (error) -> Error in
+                return NetworkError.DecodingError(error)
+            })
+            .eraseToAnyPublisher()
+    }
+    
+    private func post<T: Encodable, R: Decodable>(url: URL, data: T, result: R.Type) -> AnyPublisher<R, Error> {
+
+        return Just(data)
+            .encode(encoder: JSONEncoder())
+            .mapError { error -> Error in
+                return NetworkError.EncodingError(error)
+            }
+            .map { data -> URLRequest in
+                var request = URLRequest(url: url)
+                request.httpMethod = HTTPMethodType.post
+                request.httpBody = data
+                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.setValue(String(data.count), forHTTPHeaderField: "Content-Length")
+                
+                return request
+            }
+            .flatMap { request in
+                return self.session.dataTaskPublisher(for: request)
+                    .tryMap { element -> R in
+                        guard let httpResponse = element.response as? HTTPURLResponse,
+                              httpResponse.statusCode == 200 else {
+                            throw NetworkError.BadURL
+                        }
+                        let result = try JSONDecoder().decode(R.self, from: element.data)
+                        return result
+                    }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+}
 
