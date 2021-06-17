d1e4f61b6811652a4ff4c59962ad576e1f6ea83d
Lia <kjk0825@postech.ac.kr>
2021-06-17 16:04:32 +0900

    feat: ✨issueFilter  file 생성(#120)

---
+++ ios/IssueTracker/IssueTracker/IssueFilter/IssueFilterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  IssueFilterViewController.swift
+//  IssueTracker
+//
+//  Created by Lia on 2021/06/17.
+//
+
+import UIKit
+
+class IssueFilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
