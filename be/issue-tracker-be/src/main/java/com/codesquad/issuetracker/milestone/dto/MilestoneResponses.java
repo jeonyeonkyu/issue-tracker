a829b46d618a4eb1464fe1d54aa9fe96c9132242
hiro <kiryuinh32@gmail.com>
2021-06-22 14:38:24 +0900

    feat: MilestoneResponses dto 작성

--- be/issue-tracker-be/src/main/java/com/codesquad/issuetracker/milestone/dto/MilestoneResponses.java
+++ be/issue-tracker-be/src/main/java/com/codesquad/issuetracker/milestone/dto/MilestoneResponses.java
@@ -1,11 +1,13 @@
 package com.codesquad.issuetracker.milestone.dto;
 
+import com.codesquad.issuetracker.milestone.domain.Milestones;
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Data;
 
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data(staticConstructor = "from")
 public class MilestoneResponses {
@@ -15,5 +17,13 @@ public class MilestoneResponses {
 
     public static MilestoneResponses from(Collection<MilestoneResponse> milestoneResponses) {
         return new MilestoneResponses(new HashSet<>(milestoneResponses));
+    }
+
+    public static MilestoneResponses from(Milestones milestoneEntities) {
+        Set<MilestoneResponse> milestoneResponses = milestoneEntities.stream()
+                .map(MilestoneResponse::from)
+                .collect(Collectors.toSet());
+
+        return MilestoneResponses.from(milestoneResponses);
     }
 }
