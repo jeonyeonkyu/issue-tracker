3554c78803ee6a17dec9ea93d87f06fb223eb319
Lia <kjk0825@postech.ac.kr>
2021-06-23 00:24:50 +0900

    feat: ✨login file 생성(#142)

---
+++ ios/IssueTracker/IssueTracker/Login/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  IssueTracker
+//
+//  Created by Lia on 2021/06/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
