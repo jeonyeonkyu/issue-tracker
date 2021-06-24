5058fcaed7c2ae86de06d45e1b62f40ec80c9cab
Jisoo Kim <ghis22130@naver.com>
2021-06-24 10:21:55 +0900

    feat: ✨상세 이슈 모델 받는 UseCase(#144)

---
+++ ios/IssueTracker/IssueTracker/IssueList/UseCase/FetchIssueDetailUseCase.swift
@@ -0,0 +1,37 @@
+//
+//  FetchIssueDetailUseCase.swift
+//  IssueTracker
+//
+//  Created by 지북 on 2021/06/23.
+//
+
+import Foundation
+import Combine
+
+protocol FetchIssueDetailUseCase {
+    func excute(id: Int, completion: @escaping (Result<IssueDetail, NetworkError>) -> Void)
+
+}
+
+final class DefaultFetchIssueDetailUseCase: FetchIssueDetailUseCase {
+
+    private var networkManager: NetworkManageable
+    private var cancelBag = Set<AnyCancellable>()
+    
+    init(networkManager: NetworkManageable) {
+        self.networkManager = networkManager
+    }
+    
+    func excute(id: Int, completion: @escaping (Result<IssueDetail, NetworkError>) -> Void) {
+        networkManager.get(path: "/issues/\(id)", type: IssueDetail.self)
+            .receive(on: DispatchQueue.main)
+            .sink { error in
+                switch error {
+                case .failure(let error): completion(.failure(error))
+                case .finished: break
+                }
+            } receiveValue: { issueDetail in
+                completion(.success(issueDetail))
+            }.store(in: &cancelBag)
+    }
+}
