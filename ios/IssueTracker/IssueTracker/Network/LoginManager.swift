bbec198750c5f0a2255f639c68a3daaefbe83976
Lia <kjk0825@postech.ac.kr>
2021-06-24 15:15:51 +0900

    feat: ✨requestJWT method 작성(#142)

--- ios/IssueTracker/IssueTracker/Network/LoginManager.swift
+++ ios/IssueTracker/IssueTracker/Network/LoginManager.swift
@@ -10,6 +10,17 @@ import Combine
 
 class LoginManager {
     
+    struct JWT: Decodable {
+        let jwt: String
+    }
+    
+    @Published private var jwt: JWT!
+    @Published private var error: NetworkError!
+    private var cancelBag = Set<AnyCancellable>()
+    
+    init() {
+        self.jwt = JWT(jwt: "")
+    }
     
 }
 
@@ -20,4 +31,29 @@ extension LoginManager {
         handler(url, LoginURL.callbackUrlScheme)
     }
     
+    func requestJWT(with code: URL) {
+        let code = abstractCode(with: code)
+        let url = "http://15.164.68.136:80/api/login/auth?client=ios&code=\(code)"
+        NetworkManager().get(path: url, type: JWT.self)
+            .receive(on: DispatchQueue.main)
+            .sink { error in
+                switch error {
+                case .failure(let error): self.error = error
+                case .finished: break
+                }
+            } receiveValue: { jwt in
+                self.jwt = jwt
+            }.store(in: &cancelBag)
+    }
+    
+    private func abstractCode(with url: URL) -> String {
+        if url.absoluteString.starts(with: "issueTracker://") {
+            if let code = url.absoluteString.split(separator: "=").last.map({ String($0) }) {
+                return code
+            }
+        }
+        return ""
+    }
+    
 }
+
