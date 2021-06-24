10e8f0aac5bbedb55fed3078922e51272e76a76b
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-06-24 18:02:34 +0900

    refactor: Issue 테스트용 validator 분리

--- be/issue-tracker-be/src/test/java/com/codesquad/issuetracker/issue/service/IssueServiceTest.java
+++ be/issue-tracker-be/src/test/java/com/codesquad/issuetracker/issue/service/IssueServiceTest.java
@@ -13,7 +13,6 @@ import com.codesquad.issuetracker.issue.repository.IssueRepository;
 import com.codesquad.issuetracker.label.controller.LabelDummyData;
 import com.codesquad.issuetracker.label.domain.Label;
 import com.codesquad.issuetracker.label.domain.Labels;
-import com.codesquad.issuetracker.label.dto.LabelResponse;
 import com.codesquad.issuetracker.label.dto.LabelResponses;
 import com.codesquad.issuetracker.milestone.controller.MilestoneDummyData;
 import com.codesquad.issuetracker.milestone.dto.MilestoneResponse;
@@ -29,11 +28,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @SpringBootTest(classes = {IssueService.class})
@@ -59,7 +59,7 @@ class IssueServiceTest {
         IssueResponses actual = issueService.readAll(user);
 
         for (int i = 0; i < actual.getIssueResponses().size(); i++) {
-            thenVerifyIssue(actual.getIssueResponses().get(i), expected.getIssueResponses().get(i));
+            IssueTestValidator.thenVerifyIssue(actual.getIssueResponses().get(i), expected.getIssueResponses().get(i));
         }
     }
 
@@ -174,7 +174,7 @@ class IssueServiceTest {
 
         IssueDetailResponse actual = issueService.readOne(given.getId());
 
-        thenVerifyIssue(actual, expected);
+        IssueTestValidator.thenVerifyIssue(actual, expected);
     }
 
     @SuppressWarnings("unused")
@@ -243,7 +243,7 @@ class IssueServiceTest {
 
         IssueDetailResponse actual = issueService.create(issueRequest);
 
-        thenVerifyIssue(actual, expected);
+        IssueTestValidator.thenVerifyIssue(actual, expected);
     }
 
     @SuppressWarnings("unused")
@@ -446,110 +446,6 @@ class IssueServiceTest {
         );
     }
 
-
-    private void thenVerifyIssue(IssueResponse actual, IssueResponse expected) {
-        assertThat(actual.getId()).isEqualTo(expected.getId());
-        assertThat(actual.getNumber()).isEqualTo(expected.getNumber());
-        assertThat(actual.getTitle()).isEqualTo(expected.getTitle());
-        assertThat(actual.getDescription()).isEqualTo(expected.getDescription());
-        assertThat(actual.isClosed()).isEqualTo(expected.isClosed());
-        assertThat(actual.isHasSameAuthorComments()).isEqualTo(expected.isHasSameAuthorComments());
-        assertThat(actual.getCreateDateTime()).isEqualTo(expected.getCreateDateTime());
-
-        verifyUser(actual.getAuthor(), expected.getAuthor());
-
-        List<UserResponse> actualUserResponses = new ArrayList<>(actual.getAssignees().getUserResponses());
-        List<UserResponse> expectedUserResponses = new ArrayList<>(expected.getAssignees().getUserResponses());
-
-        for (int i = 0; i < actualUserResponses.size(); i++) {
-            verifyUser(actualUserResponses.get(i), expectedUserResponses.get(i));
-        }
-
-        List<LabelResponse> actualLabelResponses = new ArrayList<>(actual.getLabels().getLabelResponses());
-        List<LabelResponse> expectedLabelResponses = new ArrayList<>(expected.getLabels().getLabelResponses());
-
-        for (int i = 0; i < actualLabelResponses.size(); i++) {
-            verifyLabel(actualLabelResponses.get(i), expectedLabelResponses.get(i));
-        }
-
-        verifyMilestone(actual.getMilestone(), expected.getMilestone());
-    }
-
-    private void thenVerifyIssue(IssueDetailResponse actual, IssueDetailResponse expected) {
-        assertThat(actual.getId()).isEqualTo(expected.getId());
-        assertThat(actual.getNumber()).isEqualTo(expected.getNumber());
-        assertThat(actual.getTitle()).isEqualTo(expected.getTitle());
-
-        if (actual.getMainComment() != null && expected.getMainComment() != null) {
-            verifyComment(actual.getMainComment(), expected.getMainComment());
-        }
-
-        List<CommentResponse> actualCommentResponse = actual.getComments() != null ? actual.getComments().getCommentResponses() : Collections.emptyList();
-        List<CommentResponse> expectedCommentResponse = expected.getComments() != null ? expected.getComments().getCommentResponses() : Collections.emptyList();
-
-        for (int i = 0; i < actualCommentResponse.size(); i++) {
-            verifyComment(actualCommentResponse.get(i), expectedCommentResponse.get(i));
-        }
-
-        assertThat(actual.isClosed()).isEqualTo(expected.isClosed());
-        assertThat(actual.getCreateDateTime()).isEqualTo(expected.getCreateDateTime());
-
-        verifyUser(actual.getAuthor(), expected.getAuthor());
-
-        List<UserResponse> actualUserResponses = new ArrayList<>(actual.getAssignees() != null ? actual.getAssignees().getUserResponses() : Collections.emptyList());
-        List<UserResponse> expectedUserResponses = new ArrayList<>(expected.getAssignees() != null ? expected.getAssignees().getUserResponses() : Collections.emptyList());
-
-        for (int i = 0; i < actualUserResponses.size(); i++) {
-            verifyUser(actualUserResponses.get(i), expectedUserResponses.get(i));
-        }
-
-        List<LabelResponse> actualLabelResponses = new ArrayList<>(actual.getLabels().getLabelResponses());
-        List<LabelResponse> expectedLabelResponses = new ArrayList<>(expected.getLabels().getLabelResponses());
-
-        for (int i = 0; i < actualLabelResponses.size(); i++) {
-            verifyLabel(actualLabelResponses.get(i), expectedLabelResponses.get(i));
-        }
-
-        if (actual.getMilestone() != null && expected.getMilestone() != null) {
-            verifyMilestone(actual.getMilestone(), expected.getMilestone());
-        }
-    }
-
-    private void verifyUser(UserResponse actual, UserResponse expected) {
-        assertThat(actual.getId()).isEqualTo(expected.getId());
-        assertThat(actual.getEmail()).isEqualTo(expected.getEmail());
-        assertThat(actual.getName()).isEqualTo(expected.getName());
-        assertThat(actual.getProfileImage()).isEqualTo(expected.getProfileImage());
-    }
-
-    private void verifyLabel(LabelResponse actual, LabelResponse expected) {
-        assertThat(actual.getId()).isEqualTo(expected.getId());
-        assertThat(actual.getName()).isEqualTo(expected.getName());
-        assertThat(actual.getDescription()).isEqualTo(expected.getDescription());
-        assertThat(actual.getColor()).isEqualTo(expected.getColor());
-    }
-
-    private void verifyMilestone(MilestoneResponse actual, MilestoneResponse expected) {
-        assertThat(actual.getId()).isEqualTo(expected.getId());
-        assertThat(actual.getName()).isEqualTo(expected.getName());
-        assertThat(actual.getDescription()).isEqualTo(expected.getDescription());
-        assertThat(actual.getDueDate()).isEqualTo(expected.getDueDate());
-        assertThat(actual.isClosed()).isEqualTo(expected.isClosed());
-        assertThat(actual.getOpenedIssueCount()).isEqualTo(expected.getOpenedIssueCount());
-        assertThat(actual.getClosedIssueCount()).isEqualTo(expected.getClosedIssueCount());
-    }
-
-    private void verifyComment(CommentResponse actual, CommentResponse expected) {
-        assertThat(actual.getId()).isEqualTo(expected.getId());
-        assertThat(actual.getContents()).isEqualTo(expected.getContents());
-
-        verifyUser(actual.getAuthor(), expected.getAuthor());
-
-        assertThat(actual.getCreateDateTime()).isEqualTo(expected.getCreateDateTime());
-
-        assertThat(actual.getEmojis()).isEqualTo(expected.getEmojis());
-    }
-
     @ParameterizedTest
     @MethodSource
     void update(String 테스트케이스설명, Issue givenForReadById, Issue givenForSave, IssueRequest issueRequest, IssueDetailResponse expected) {
@@ -561,7 +457,7 @@ class IssueServiceTest {
 
         IssueDetailResponse actual = issueService.update(givenForReadById.getId(), issueRequest);
 
-        thenVerifyIssue(actual, expected);
+        IssueTestValidator.thenVerifyIssue(actual, expected);
     }
 
     @SuppressWarnings("unused")
